990979645a46967008bb5d25e823a25f66f1ca1a
Paige Jones <hello@paigejones.me>
2022-06-01 21:22:22 +0100

    feat: add admin dashboard

---
+++ src/Facades/AdminDashboard.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace HauntPet\Framework\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class AdminDashboard extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'haunt-admin-dashboard';
+    }
+}
