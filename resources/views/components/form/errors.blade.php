7f4a8f5d5df195b53d8e39cd865a2026194fb29e
Paige Jones <hello@paigejones.me>
2022-06-07 20:28:18 +0100

    refactor: make errors optionally shown

--- resources/views/components/form/errors.blade.php
+++ resources/views/components/form/errors.blade.php
@@ -1,5 +1,7 @@
-<div class="-mt-3 text-red-500 text-xs">
-    @foreach($errors->get($attributes['name']) as $error)
-        <div>{{ $error }}</div>
-    @endforeach
-</div>
+@if($errors->has($attributes['name']))
+    <div class="-mt-3 text-red-500 text-xs">
+        @foreach($errors->get($attributes['name']) as $error)
+            <div>{{ $error }}</div>
+        @endforeach
+    </div>
+@endif
