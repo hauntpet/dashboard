57d1ef89b6ec59487b3a16bf6c04e0a6f943a77f
Paige Jones <hello@paigejones.me>
2022-06-14 16:09:56 +0100

    fix: check active exists in topbar

--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -1,9 +1,11 @@
 <x-haunt::card :applyOverflow="false" buffer="small" :level="0" :showBorder="true" :showMargin="false" :applyRounded="false">
     <x-haunt::card.content class="flex items-center px-6 rounded-none">
         <div class="flex flex-grow font-bold space-x-3 text-gray-400 text-sm uppercase">
-            @foreach(AdminDashboard::getActive()['children'] as $item)
-                <x-haunt::link class="hover:text-blue-500" :content="$item['title']" :href="AdminDashboard::handleRoute($item['route'])" />
-            @endforeach
+            @if(AdminDashboard::getActive())
+                @foreach(AdminDashboard::getActive()['children'] as $item)
+                    <x-haunt::link class="hover:text-blue-500" :content="$item['title']" :href="AdminDashboard::handleRoute($item['route'])" />
+                @endforeach
+            @endif
         </div>
         <div class="flex items-center space-x-2">
             <div class="leading-3 text-right">
